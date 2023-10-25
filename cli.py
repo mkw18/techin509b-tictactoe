e6fc28b17c151e6320b87a4452b40574002a8dd0
mkw18 <mkw22@mails.tsinghua.edu.cn>
2023-10-25 15:20:39 -0700

    add function

--- cli.py
+++ cli.py
@@ -8,10 +8,18 @@ from logic import make_empty_board
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
+    current_user = 'X'
     while winner == None:
-        print("TODO: take a turn!")
+        print(f"TODO: {current_user} take a turn!")
         # TODO: Show the board to the user.
+        for row in board:
+            print(row)
         # TODO: Input a move from the player.
+        movement = input('Your next move (format: x,y): ').split(',')
+        x = int(movement[0].strip())
+        y = int(movement[1].strip())
         # TODO: Update the board.
+        board[x][y] = current_user
         # TODO: Update who's turn it is.
+        current_user = 'XO'.replace(current_user, '')
         winner = 'X'  # FIXME
