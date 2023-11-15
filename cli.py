6b1a715b7db78ac2625a28502777ed6ca5a35452
mkw18 <mkw22@mails.tsinghua.edu.cn>
2023-11-14 22:30:51 -0800

    OOP

--- cli.py
+++ cli.py
@@ -2,31 +2,26 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board, get_winner, other_player
+from logic import Game, Board, Human, Bot
 
 
 # Reminder to check all the tests
 
 if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
-    current_user = 'X'
-    while winner == None:
-        print(f"TODO: {current_user} take a turn!")
-        # TODO: Show the board to the user.
-        for row in board:
-            print(row)
-        # TODO: Input a move from the player.
-        movement = input(f'Player {current_user}, please enter your next move, \
-                         ranging from 0 to 2 (format: x,y): ').split(',')
-        x = int(movement[0].strip())
-        y = int(movement[1].strip())
-        # TODO: Update the board.
-        board[x][y] = current_user
-        # TODO: Update who's turn it is.
-        current_user = other_player(current_user)
-        winner =  get_winner(board)
-    print(f'The winner is: {winner}')
-    print('The final board is:')
-    for row in board:
-        print(row)
+    mode = input("Please select the game mode, 1 for single player mode, and 2 for two player mode: ")
+    assert mode == '1' or mode == '2'
+    if mode == '1':
+        user_player = input("Please select the player you want to be, input X or O: ")
+        assert user_player == 'X' or user_player == 'O'
+        if user_player == 'X':
+            playerX = Human('X')
+            playerO = Bot('O')
+        else:
+            playerX = Bot('X')
+            playerO = Human('O')
+    else:
+        playerX = Human('X')
+        playerO = Human('O')
+    game = Game(playerX, playerO)
+    game.run()
+    
--- logic.py
+++ logic.py
@@ -2,37 +2,113 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
-def make_empty_board():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+import random
 
 
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    # Decide if 'O' won
-    if board[0] == ['O', 'O', 'O'] or board[1] == ['O', 'O', 'O'] or board[2] == ['O', 'O', 'O']\
-    or [board[0][0], board[1][0], board[2][0]] == ['O', 'O', 'O'] or [board[0][1], board[1][1], board[2][1]] == ['O', 'O', 'O']\
-    or [board[0][2], board[1][2], board[2][2]] == ['O', 'O', 'O']\
-    or [board[0][0], board[1][1], board[2][2]] == ['O', 'O', 'O'] or [board[0][2], board[1][1], board[2][0]] == ['O', 'O', 'O']:
-        return 'O'
-    # Decide if 'X' won
-    elif board[0] == ['X', 'X', 'X'] or board[1] == ['X', 'X', 'X'] or board[2] == ['X', 'X', 'X']\
-    or [board[0][0], board[1][0], board[2][0]] == ['X', 'X', 'X'] or [board[0][1], board[1][1], board[2][1]] == ['X', 'X', 'X']\
-    or [board[0][2], board[1][2], board[2][2]] == ['X', 'X', 'X']\
-    or [board[0][0], board[1][1], board[2][2]] == ['X', 'X', 'X'] or [board[0][2], board[1][1], board[2][0]] == ['X', 'X', 'X']:
-        return 'X'
-    # Otherwise draw
-    else:
-        for l in board:
-            if None in l:
+class Board:
+    def __init__(self):
+        self._rows = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
+        self.row = 3
+        self.col = 3
+
+    def __str__(self):
+        s = '-------\n'
+        for row in self._rows:
+            for cell in row:
+                s = s + '|'
+                if cell == None:
+                    s=s+' '
+                else:
+                    s=s+cell
+            s = s + '|\n-------\n'
+        return s
+
+    def get(self, x, y):
+        return self._rows[x][y]
+
+    def set(self, x, y, value):
+        self._rows[x][y] = value
+
+    def get_winner(self):
+        for row in self._rows:
+            if len(set(row)) == 1 and row[0]:
+                return row[0]
+        for j in range(len(self._rows[0])):
+            if len(set([self._rows[i][j] for i in range(len(self._rows))])) == 1 and self._rows[0][j]:
+                return self._rows[0][j]
+        if len(set([self._rows[i][i] for i in range(len(self._rows))])) == 1 and self._rows[0][0]:
+            return self._rows[0][0]
+        if len(set([self._rows[i][len(self._rows)-i-1] for i in range(len(self._rows))])) == 1\
+            and self._rows[0][len(self._rows[0])-1]:
+            return self._rows[0][len(self._rows[0])-1]
+
+        for row in self._rows:
+            if None in row:
                 return None
         return 'Draw'
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "XO".replace(player, '')
+
+class Game:
+    def __init__(self, playerX, playerO):
+        self._board = Board()
+        self._playerX = playerX
+        self._playerO = playerO
+        self._current_player = playerX
+
+    def other_player(self):
+        """Given the character for a player, returns the other player."""
+        if self._current_player == self._playerX:
+            return self._playerO
+        return self._playerX
+
+    def run(self):
+        while self._board.get_winner() == None:
+            self._current_player.get_move(self._board)
+            print(self._board)
+            self._current_player = self.other_player()
+
+        winner = self._board.get_winner()
+        if not winner == 'Draw':
+            print(f'The winner is: {winner}')
+        else:
+            print('Draw!')
+        # print('The final board is:')
+        # print(self._board)
+
+
+class Human:
+    def __init__(self, id):
+        self.id = id
+
+    def get_move(self, board):
+        while True:
+            movement = input(f'Player {self.id}, please enter your next move, ranging from 0 to 2 (format: x,y): ').split(',')
+            x = int(movement[0].strip())
+            y = int(movement[1].strip())
+            if board.get(x,y) == None:
+                board.set(x, y, self.id)
+                break
+            else:
+                print('Occupied position, please try again.')
+        return board
+
+
+class Bot:
+    def __init__(self, id):
+        self.id = id
+
+    def get_move(self, board):
+        available_pos = []
+        for i in range(board.row):
+            for j in range(board.col):
+                if board.get(i,j) == None:
+                    available_pos.append((i,j))
+        x, y = random.choices(available_pos)[0]
+        print(f"Player {self.id}'s next move is {x, y}.")
+        board.set(x, y, self.id)
+        return board
+  
