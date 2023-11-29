1a91f27000ff5c1bc1c9e5ead0069302211dbed4
mkw18 <mkw22@mails.tsinghua.edu.cn>
2023-11-28 18:44:33 -0800

    add loop

--- cli.py
+++ cli.py
@@ -17,26 +17,32 @@ logger.setLevel(logging.INFO)
 # Reminder to check all the tests
 
 if __name__ == '__main__':
-    mode = input("Please select the game mode, 1 for single player mode, and 2 for two player mode: ")
-    # assert mode == '1' or mode == '2'
-    if mode == '1':
-        logger.info('Single player mode')
-        user_player = input("Please select the player you want to be, input X or O: ")
-        assert user_player == 'X' or user_player == 'O'
-        if user_player == 'X':
+    while True:
+        mode = input("Please select the game mode, 1 for single player mode, and 2 for two player mode: ")
+        # assert mode == '1' or mode == '2'
+        if mode == '1':
+            logger.info('Single player mode')
+            while True:
+                user_player = input("Please select the player you want to be, input X or O: ")
+                # assert user_player == 'X' or user_player == 'O'
+                if user_player == 'X':
+                    playerX = Human('X')
+                    playerO = Bot('O')
+                    break
+                elif user_player == 'O':
+                    playerX = Bot('X')
+                    playerO = Human('O')
+                    break
+                else:
+                    print('Please input X or O')
+            break
+        elif mode == '2':
+            logger.info('Two player mode')
             playerX = Human('X')
-            playerO = Bot('O')
-        else:
-            playerX = Bot('X')
             playerO = Human('O')
-    elif mode == '2':
-        logger.info('Two player mode')
-        playerX = Human('X')
-        playerO = Human('O')
-    else:
-        logger.info('Two player mode')
-        playerX = Bot('X')
-        playerO = Bot('O')
+            break
+        else:
+            print('Please input 1 or 2')
     game = Game(playerX, playerO)
     winner = game.run(logger)
     logger.info(winner)
